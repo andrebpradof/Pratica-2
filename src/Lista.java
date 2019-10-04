7fc843e2bb48f5dec28674344e8709d13a607ee7
Andre Furlanetti <andrebpf@usp.br>
2019-10-04 15:05:07 -0300

    Algumas alterações

--- src/Lista.java
+++ src/Lista.java
@@ -2,7 +2,7 @@ public abstract class Lista implements Imprimivel{
     protected int tamanho;
 
     public int tamanho(){
-
+        return 1;
     }
 
     public abstract void inserir(IElemento elem);
