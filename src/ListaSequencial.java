71b4645748a289c4eccb390e20762b99d57516f0
modest0w <56058578+modest0w@users.noreply.github.com>
2019-10-04 18:12:54 -0300

    classes inserir e remover da lista sequencial atualizadas(1)

--- src/ListaSequencial.java
+++ src/ListaSequencial.java
@@ -1,15 +1,26 @@
 public class ListaSequencial extends Lista {
     private IElemento data[];
 
+    @Override
     public void inserir(IElemento elem){
-
+        tamanho++;
+        data[tamanho]=elem;
     }
 
+    @Override
     public void remover(IElemento elem){
-
+        int i;
+        for (i=0; i<tamanho; i++){
+            if(elem.equals(data[i])){
+                while(i<tamanho){
+                    data[i]=data[++i];
+                }
+                data[i]=null;
+            }
+        }
     }
 
-    public void imprimir(){
+    public void imprimir(IElemento){
 
     }
 }
